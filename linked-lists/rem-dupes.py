209a5608b4dc407f65d4ba41b149b50b9045dab8
motleytech <motleytechnet@gmail.com>
2017-03-25 09:59:55 -0700

    refactor code - add linked list creation method, and rename remDupes -> removeDuplicates

--- linked-lists/rem-dupes.py
+++ linked-lists/rem-dupes.py
@@ -1,8 +1,17 @@
 from llist import LinkedList, Node
 from random import randint
 
+def createList(N, M):
+    '''
+    Create a linked list containing N nodes,
+    and each node consisting of a random number upto M
+    '''
+    lst = LinkedList()
+    [lst.add(Node(randint(1, M+1))) for x in range(N)]
+    return lst
 
-def remDupes(lst):
+
+def removeDuplicates(lst):
     '''
     remove duplicates nodes from a linked list
     '''
@@ -20,10 +29,7 @@ def numDupes(lst):
     nodeList = [node.data for node in lst]
     return len(nodeList) - len(set(nodeList))
 
-
-lst = LinkedList()
-[lst.add(Node(randint(1,50))) for x in range(100)]
-
+lst = createList(100, 100)
 print "The list currently has %s duplicates" % numDupes(lst)
-remDupes(lst)
+removeDuplicates(lst)
 print "The list now has %s duplicates" % numDupes(lst)
