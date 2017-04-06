cb9eed3dc16b28ba9e7e2a1559557cd41dd1b885
MotleyTech <motleytechnet@gmail.com>
2017-04-05 21:06:38 -0700

    palindrome recursive version... still using extra storage in a sense

--- linked-lists/palindrome.py
+++ linked-lists/palindrome.py
@@ -7,11 +7,20 @@ def isPalindrome(lst):
             return False
     return True
 
+def isPalin2(lst, curr):
+    if curr.next is None:
+        return (curr.data == lst.data, lst.next)
+    res, node = isPalin2(lst, curr.next)
+    if not res:
+        return (False, None)
+    return (node.data == curr.data, node.next)
+
+
 lst = LinkedList()
 lst.add(1).add(2).add(3)
-print isPalindrome(lst)
+print isPalindrome(lst), isPalin2(lst.head, lst.head)[0]
 lst.add(2).add(1)
 
-print isPalindrome(lst)
+print isPalindrome(lst), isPalin2(lst.head, lst.head)[0]
 
 
