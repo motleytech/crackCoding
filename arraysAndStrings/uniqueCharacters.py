63a48dfe0cbae015869a66fe426eaa123812dde7
MotleyTech <motleytechnet@gmail.com>
2017-04-07 14:56:57 -0700

    clean up uniqueChars code

--- arraysAndStrings/uniqueCharacters.py
+++ arraysAndStrings/uniqueCharacters.py
@@ -3,6 +3,10 @@ method to check if the given string has all unique chars
 '''
 
 def hasUniqueChars(s):
+    '''
+    checks if a string is composed of unique characters
+    (uses a set)
+    '''
     existing = set()
     for c in s:
         if c in existing:
@@ -11,6 +15,10 @@ def hasUniqueChars(s):
     return True
 
 def hasUniqueCharsNoBuf(s):
+    '''
+    checks if a string consists of unique characters
+    This version uses no extra storage (like a set or dict(hashmap))
+    '''
     ls = len(s)
     for x in range(ls - 1):
         for y in range(x+1, ls):
@@ -18,19 +26,13 @@ def hasUniqueCharsNoBuf(s):
                 return False
     return True
 
-def testBufMethod():
-    huc = hasUniqueChars
+def testMethod(func):
+    huc = func
     assert(huc('abcde') == True)
     assert(huc('abcdea') == False)
     assert(huc('aa') == False)
-    print 'test 1 passed'
+    print 'test passed: %s' % func.__name__
 
-def testNonBufMethod():
-    hnb = hasUniqueCharsNoBuf
-    assert(hnb('abcde') == True)
-    assert(hnb('abcdea') == False)
-    assert(hnb('aa') == False)
-    print 'test 2 passed'
 
-testBufMethod()
-testNonBufMethod()
+testMethod(hasUniqueChars)
+testMethod(hasUniqueCharsNoBuf)
