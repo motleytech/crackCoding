e51fb6891f083b3beea1de30c4dbc75c46673bf9
MotleyTech <motleytechnet@gmail.com>
2017-04-07 16:29:46 -0700

    clean up code for checking permutations

--- arraysAndStrings/isPermutation.py
+++ arraysAndStrings/isPermutation.py
@@ -1,16 +1,35 @@
+'''
+Given two strings, check if one is a permutation of the other. Permutations of this kind are also called anagrams.
 
-def createFreqMap(s):
-    fmap = {}
+The idea is to create a character freq map (using a dict) of the two strings
+and compare them.
+
+Note: We will ignore the spaces in the string, so that "i am lord voldemort"
+is indeed a permutation of "tom marvolo riddle"
+'''
+
+from collections import defaultdict
+
+def createFreqMap(s, ignoreSpaces=True):
+    '''
+    create a freq map of the characters occuring in s
+
+    '''
+    fmap = defaultdict(lambda : 0)
     for c in s:
-        if c in fmap:
+        if c != ' ':
             fmap[c] += 1
-        else:
-            fmap[c] = 1
     return fmap
 
-def compareMaps(m1, m2):
+def areFreqMapsSame(m1, m2):
+    '''
+    Compare 2 frequency maps and return True if they are same.
+    '''
+    # compare number of keys... must have same number of keys
+    # number of keys == number of unique characters in each string
     if len(m1) != len(m2):
         return False
+    # is freq of occurence of each character the same in both
     for k, v in m1.items():
         if k not in m2:
             return False
@@ -19,16 +38,21 @@ def compareMaps(m1, m2):
     return True
 
 def isPermutation(s1, s2):
-    if len(s1) != len(s2):
-        return False
-    return compareMaps(createFreqMap(s1), createFreqMap(s2))
+    '''
+    check if s1 and s2 are anagrams of each other (contain the same characters)
+    '''
+    return areFreqMapsSame(createFreqMap(s1), createFreqMap(s2))
 
 def testIsPerm():
-    assert(isPermutation('abcd', 'cdba') == True)
-    assert(isPermutation('bbbb', 'bbbb') == True)
-    assert(isPermutation('ab', 'ba') == True)
-    assert(isPermutation('abcd', 'cdbae') == False)
-    assert(isPermutation('abcd', 'cdbe') == False)
+    '''
+    test for our implementation
+    '''
+    assert isPermutation('abcd', 'cdba') == True
+    assert isPermutation('bbbb', 'bbbb') == True
+    assert isPermutation('ab', 'ba') == True
+    assert isPermutation('abcd', 'cdbae') == False
+    assert isPermutation('abcd', 'cdbe') == False
+    assert isPermutation('tom marvolo riddle', 'i am lord voldemort')
     print 'test passed'
 
 testIsPerm()
