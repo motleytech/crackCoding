276bed75fe502b91f2ef558be6d264ca4f9a71f8
Motleytech <motleytechnet@gmail.com>
2017-10-19 15:15:47 -0700

    solution for boolean evaluation problem

---
+++ RecursionAndDyn/booleanEval.py
@@ -0,0 +1,98 @@
+def evaluate(exp):
+    if len(exp) != 3:
+        raise Exception("expression length must be 3: %s" % exp)
+
+    a, op, b = exp
+    a, b = int(a), int(b)
+
+    if op not in ['&', '|', '^']:
+        raise Exception("Operation unrecognized: %s" % exp)
+
+    if op == '&':
+        return a & b
+    if op == '|':
+        return a | b
+    if op == '^':
+        return a ^ b
+
+    raise Exception("Error evaluating expression : %s" % exp)
+
+def countWays(k, cache2):
+    if k in cache2:
+        return cache2[k]
+    if k <= 1:
+        return 1
+    nways = 0
+    for x in range(0, k):
+        nways += countWays(x, cache2) * countWays(k-x-1, cache2)
+    cache2[k] = nways
+    return nways
+
+
+def countBoolEval(exp, cache1, cache2):
+    if exp in cache1:
+        return cache1[exp]
+    if len(exp) == 3:
+        res = evaluate(exp)
+        cache1[exp] = { 1: res, 0: 0 if res else 1 }
+        return cache1[exp]
+    else:
+        numOps = len(exp) / 2
+        nways = {1: 0, 0: 0}
+        for x in range(0, numOps):
+            y = x*2 + 1
+            first, op, second = exp[:y], exp[y], exp[y+1:]
+            if len(first) > len(second):
+                first, second = second, first
+            if len(first) == 1:
+                if first == '1':
+                    res1 = { 1: 1, 0: 0 }
+                else:
+                    res1 = { 1: 0, 0: 1 }
+            else:
+                # evaluate first and second and merge results
+                res1 = countBoolEval(first, cache1, cache2)
+
+            # use these shortcircuits
+            # 1 |
+            # 0 &
+            done = False
+            if res1[0] == 0 and op == '|':
+                nways[1] += res1[1] * countWays(len(second)/2, cache2)
+                done = True
+            elif res1[1] == 0 and op == '&':
+                nways[0] += res1[0] * countWays(len(second)/2, cache2)
+                done = True
+            if done:
+                continue
+
+            res2 = countBoolEval(second, cache1, cache2)
+            res1c, res2c = sum(res1.values()), sum(res2.values())
+            if op == '|':
+                bothzero = res1[0] * res2[0]
+                nways[0] += bothzero
+                nways[1] += res1c * res2c - bothzero
+            elif op == '&':
+                bothone = res1[1] * res2[1]
+                nways[1] += bothone
+                nways[0] += res1c * res2c - bothone
+            elif op == '^':
+                bothsame = res1[0] * res2[0] + res1[1] * res2[1]
+                nways[0] += bothsame
+                nways[1] += res1c * res2c - bothsame
+        cache2[exp] = nways
+        return nways
+
+
+cache2 = {}
+for x in range(20):
+    print x, countWays(x, cache2)
+
+
+cache1, cache2 = {}, {}
+expression = '1^0|0|1'
+print "%s :: %s" % (expression, countBoolEval(expression, cache1, cache2)[1])
+
+expression = '0|0&0&0&1^1'
+print "%s :: %s" % (expression, countBoolEval(expression, cache1, cache2)[1])
+
