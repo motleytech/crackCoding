dce21f14d6eda04a5f93e8d70150d9ffa989bf0c
motleytech <motleytechnet@gmail.com>
2018-04-13 20:48:40 -0700

    adding solution for calculator

--- moderate/calculator.py
+++ moderate/calculator.py
@@ -1,6 +1,18 @@
+'''implementation of a simple calculator'''
 
+operMap = {
+    '*': lambda a, b: a*b,
+    '/': lambda a, b: a/b,
+    '+': lambda a, b: a+b,
+    '-': lambda a, b: a-b
+}
+
+digits = '0123456789'
+
+opers = '+-*/'
 
 def tokenize(exp):
+    'separate expression into tokens'
     exp = exp.replace(' ', '')
     out = []
 
@@ -10,41 +22,42 @@ def tokenize(exp):
         if c == lexp:
             break
 
-        if exp[c] in '0123456789':
+        if exp[c] in digits:
             num = ''
-            while exp[c] in '0123456789':
+            while exp[c] in digits:
                 num += exp[c]
                 c += 1
                 if c == lexp:
                     break
-
             out.append(num)
-        elif exp[c] in '+-/*':
+
+        elif exp[c] in opers:
             out.append(exp[c])
             c += 1
     return out
 
-
 def calc(exp):
+    'evaluate the expression'
     tokens = tokenize(exp)
 
-    res = []
-    ltokens = len(tokens)
-    i = 0
-    while i < ltokens - 2:
-        a, b, c = tokens[i], tokens[i+1], tokens[i+2]
-        if b == '*':
-            res.append(object)
+    for oset in ['*/', '+-']:
+        i = 0
+        while i < len(tokens) - 2:
+            a, b, c = [tokens[i+x] for x in range(3)]
+            if b in oset:
+                _ = [tokens.pop(i) for _ in range(3)]
+                tokens.insert(i, operMap[b](int(a), int(c)))
+            else:
+                i += 2
 
-
-
-
-
-
+    return tokens[0]
 
 def test():
-    exp = '22*3+5/6*3+15'
-    print tokenize(exp)
+    'test for the calculator'
+    exp = '22*3+14/6*3+15'
+    assert calc(exp) == eval(exp)
+
+    print 'Passed'
 
 if __name__ == '__main__':
     test()
