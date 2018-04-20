201eeccdb3cfac29f11ef99e159c0ac3530d23c3
MotleyTech <motleytechnet@gmail.com>
2018-04-20 11:33:36 -0700

    adding solution for the shortest supersequence

--- hard/shortestSuper.py
+++ hard/shortestSuper.py
@@ -1,11 +1,12 @@
+'solution for the shortest supersequence problem'
 
 from collections import defaultdict
 
 def setDiff(d1, d2):
+    'return elements in the diff of d1 and d2'
     res = set()
     for x, y in d1.items():
-        z = d2[x]
-        if y - z > 0:
+        if y > d2[x]:
             res.add(x)
     return res
 
@@ -16,20 +17,24 @@ def findMinSubseq(a, b):
     score = defaultdict(lambda: 0)
     minSeq = (len(b), 0, len(b))
     for i, x in enumerate(b):
-        score[x] += 1
         if x in aset:
-            nscore = score.copy()
+            cscore = score.copy()
+            score[x] += 1
             for j, oscore in reversed(info):
-                if setDiff(nscore, oscore) == aset:
+                if setDiff(score, oscore) == aset:
                     if i - j < minSeq[0]:
                         minSeq = (i - j, j, i)
                     break
-            info.append((i, nscore))
-    return minSeq
+            info.append((i, cscore))
+    return minSeq[1:]
 
 def test():
     'test for method findMinSubseq'
-    pass
+    a = [1, 5, 9]
+    b = [7, 5, 9, 0, 2, 1, 3, 5, 7, 9, 1, 1, 5, 8, 8, 9, 7]
+    assert findMinSubseq(a, b) == (7, 10)
+
+    print 'Passed'
 
 if __name__ == '__main__':
     test()
